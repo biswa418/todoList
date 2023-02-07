3622f0f477171bf5505cc638033f46dc85b30159
biswa418 <biswajeetsutar418@gmail.com>
2023-02-08 02:53:30 +0530

    First commit - basic setup express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(`Could not start the server: ${err}`);
+        return;
+    }
+
+    console.log(`Server is started on port: ${port}`);
+});
